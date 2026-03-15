3b8f197b5e67dd0dd24b2392e9cf6dc7a80c4a90
watilapm <watilapm@gmail.com>
2026-03-15 17:58:14 -0300

    chore: remove arquivos locais e scripts de teste do versionamento

--- teste_loader.py
+++
@@ -1,7 +0,0 @@
-from modules.utils.loader_processos import carregar_processos
-
-processos = carregar_processos("sislic-licencas.csv")
-
-print("Processos carregados:", len(processos))
-
-print(processos[0])
